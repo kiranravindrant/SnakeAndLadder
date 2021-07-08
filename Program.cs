cb34713eff7027ccaa0b7794859439b9489cfea4
kiranravindrant <kiranravindrant@gmail.com>
2021-07-08 13:35:23 +0530

    [kiran]Added Player at start Position

--- Program.cs
+++ Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-namespace SnakeAndLadder2
+namespace SnakeAndLadder
 {
-    class Program
+    class SnakeAndLadder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Snake And Ladder");
+            int position = 0;
+            int winningPosition = 100;
+            int dieRoll;
+            Console.WriteLine("Welcome to Snake & Ladder");
+            Console.WriteLine("Player is at Position:" + position);
+            //Player Rolls dice now
+            Random random = new Random();
+            dieRoll = random.Next(6) + 1;
+            Console.WriteLine(dieRoll);
+
+            Console.ReadLine();
+
+
+
         }
     }
 }
+
+
